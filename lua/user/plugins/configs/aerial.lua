efd81c250868853cd079460dab9a3f3445d42f5d
xuanhung1509 <vuxuanhung15092002@gmail.com>
2023-10-15 11:50:30 +0700

    fix(aerial): change backend order

--- lua/user/plugins/configs/aerial.lua
+++ lua/user/plugins/configs/aerial.lua
@@ -1,26 +1,12 @@
 return {
   config = function()
     require("aerial").setup({
+      backends = { "lsp", "treesitter", "markdown", "man" },
       layout = {
         width = 30,
         placement = "edge",
         default_direction = "right",
         preserve_equality = true,
-      },
-      filter_kind = {
-        "Class",
-        "Constant",
-        "Constructor",
-        "Enum",
-        "Function",
-        "Interface",
-        "Method",
-        "Module",
-        "Namespace",
-        "Object",
-        "Property",
-        "Struct",
-        "Variable",
       },
       attach_mode = "global",
       show_guides = true,
