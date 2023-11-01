51daae51507bc736c662fd25279c475bf6d2bc48
xuanhung1509 <vuxuanhung15092002@gmail.com>
2023-11-01 22:32:20 +0700

    fix: only remap key bindings in vscode

--- lua/user/core/keymaps/shared.lua
+++ lua/user/core/keymaps/shared.lua
@@ -1,4 +1,5 @@
 local gmap = require("user.utils").map()
+local vscode = vim.g.vscode
 
 -- Map leader to space
 vim.g.mapleader = " "
@@ -6,19 +7,19 @@ vim.g.maplocalleader = " "
 gmap({ "n", "v" }, "<Space>", "<Nop>", nil, { silent = true })
 
 -- Better default behavior
-gmap("n", "k", "v:count == 0 ? 'gk' : 'k'", nil, { expr = true, silent = true, remap = true })
-gmap("n", "j", "v:count == 0 ? 'gj' : 'j'", nil, { expr = true, silent = true, remap = true })
+gmap("n", "k", "v:count == 0 ? 'gk' : 'k'", nil, { expr = true, silent = true, remap = vscode })
+gmap("n", "j", "v:count == 0 ? 'gj' : 'j'", nil, { expr = true, silent = true, remap = vscode })
 gmap("v", "p", "P", "Better paste")
 gmap("n", "J", "mzJ`z", "Retain cursor position on merge")
 
 -- Searching and occurrences
 gmap("c", "<cr>", function()
   return vim.fn.getcmdtype() == "/" and "<cr>zzzv" or "<cr>"
-end, "Center first search result", { expr = true, remap = true })
-gmap("n", "*", "*zzzv", "Center next occurrence", { remap = true })
-gmap("n", "#", "#zzzv", "Center previous occurrence", { remap = true })
-gmap("n", "n", "nzzzv", "Center next result", { remap = true })
-gmap("n", "N", "Nzzzv", "Center previous result", { remap = true })
+end, "Center first search result", { expr = true, remap = vscode })
+gmap("n", "*", "*zzzv", "Center next occurrence", { remap = vscode })
+gmap("n", "#", "#zzzv", "Center previous occurrence", { remap = vscode })
+gmap("n", "n", "nzzzv", "Center next result", { remap = vscode })
+gmap("n", "N", "Nzzzv", "Center previous result", { remap = vscode })
 
 gmap({ "n", "i" }, "<esc>", "<cmd> nohlsearch <cr><esc>", "Escape and clear search highlights")
 gmap("v", "<", "<gv", "Indent left")
