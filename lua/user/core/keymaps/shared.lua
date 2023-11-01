288071f577cb8df347b4308a4e958efbb1cef72c
xuanhung1509 <vuxuanhung15092002@gmail.com>
2023-11-01 20:29:30 +0700

    fix: center search results and occurrences

--- lua/user/core/keymaps/shared.lua
+++ lua/user/core/keymaps/shared.lua
@@ -10,8 +10,16 @@ gmap("n", "k", "v:count == 0 ? 'gk' : 'k'", nil, { expr = true, silent = true, r
 gmap("n", "j", "v:count == 0 ? 'gj' : 'j'", nil, { expr = true, silent = true, remap = true })
 gmap("v", "p", "P", "Better paste")
 gmap("n", "J", "mzJ`z", "Retain cursor position on merge")
-gmap("n", "n", "nzzzv", "Center search result (down)", { remap = true })
-gmap("n", "N", "Nzzzv", "Center search result (up)", { remap = true })
+
+-- Searching and occurrences
+gmap("c", "<cr>", function()
+  return vim.fn.getcmdtype() == "/" and "<cr>zzzv" or "<cr>"
+end, "Center first search result", { expr = true, remap = true })
+gmap("n", "*", "*zzzv", "Center next occurrence", { remap = true })
+gmap("n", "#", "#zzzv", "Center previous occurrence", { remap = true })
+gmap("n", "n", "nzzzv", "Center next result", { remap = true })
+gmap("n", "N", "Nzzzv", "Center previous result", { remap = true })
+
 gmap({ "n", "i" }, "<esc>", "<cmd> nohlsearch <cr><esc>", "Escape and clear search highlights")
 gmap("v", "<", "<gv", "Indent left")
 gmap("v", ">", ">gv", "Indent right")
