94b528a1e9d75f979bb9cbe5d81a598d04d5becc
xuanhung1509 <vuxuanhung15092002@gmail.com>
2024-06-04 20:24:50 +0700

    fix(conform): update conform, add vue formatters

--- lua/user/plugins/configs/conform.lua
+++ lua/user/plugins/configs/conform.lua
@@ -14,6 +14,7 @@ return {
       javascriptreact = { { "prettierd", "prettier" } },
       typescript = { { "prettierd", "prettier" } },
       typescriptreact = { { "prettierd", "prettier" } },
+      vue = { { "prettierd", "prettier" } },
     },
     format_on_save = {
       timeout_ms = 500,
