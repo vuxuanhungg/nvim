01aa1d4254fb07628cc3a70c5610c74583064884
xuanhung1509 <vuxuanhung15092002@gmail.com>
2024-06-11 14:36:02 +0700

    fix(volar): disable some capabilities
    let vtsls handle them
    so no more duplicates when using trouble

--- lua/user/plugins/configs/lsp.lua
+++ lua/user/plugins/configs/lsp.lua
@@ -160,8 +160,11 @@ return {
         clangd = {},
         prismals = {},
         volar = {
-          on_attach = function()
+          on_attach = function(client)
             -- let vtsls handle instead
+            client.server_capabilities.definitionProvider = false
+            client.server_capabilities.implementationProvider = false
+            client.server_capabilities.referencesProvider = false
           end,
         },
         vtsls = {},
