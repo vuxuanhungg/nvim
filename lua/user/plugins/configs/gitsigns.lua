b0f2668d0291f03d92d6fdc84e47f74d79ee1980
xuanhung1509 <vuxuanhung15092002@gmail.com>
2024-06-13 13:38:45 +0700

    fix(gitsigns): use default signs

--- lua/user/plugins/configs/gitsigns.lua
+++ lua/user/plugins/configs/gitsigns.lua
@@ -1,13 +1,5 @@
 return {
   opts = {
-    signs = {
-      add = { text = "▎" },
-      change = { text = "▎" },
-      delete = { text = "" },
-      topdelete = { text = "" },
-      changedelete = { text = "▎" },
-      untracked = { text = "▎" },
-    },
     current_line_blame_opts = {
       delay = 300,
     },
