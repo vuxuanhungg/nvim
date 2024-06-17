bb1f50775a44a2a3df9cc329790a16adfa5f194d
xuanhung1509 <vuxuanhung15092002@gmail.com>
2024-06-17 21:08:58 +0700

    fix(alpha): cd to config dir on open config file

--- lua/user/plugins/configs/alpha.lua
+++ lua/user/plugins/configs/alpha.lua
@@ -17,8 +17,8 @@ return {
       dashboard.button("f", "󰱼" .. "  Find file", ":Telescope find_files <cr>"),
       dashboard.button("o", "󰤘" .. "  Recent files", ":Telescope oldfiles <cr>"),
       dashboard.button("w", "󰭷" .. "  Search text", ":Telescope live_grep <cr>"),
-      dashboard.button("c", "󰒓" .. "  Config", ":e $MYVIMRC <cr>"),
-      dashboard.button("q", "󰶞" .. "  Quit", ":qa<cr>"),
+      dashboard.button("c", "󰒓" .. "  Config", ":e $MYVIMRC <bar> cd %:p:h <cr>"),
+      dashboard.button("q", "󰶞" .. "  Quit", ":qa <cr>"),
     }
     dashboard.config.layout[1].val = vim.fn.max({ 2, vim.fn.floor(vim.fn.winheight(0) * 0.2) })
     dashboard.config.layout[3].val = 5
