d138f59cc2225d992b7f75b4978981b17408a2e4
xuanhung1509 <vuxuanhung15092002@gmail.com>
2024-06-26 23:10:45 +0700

    chore: format space

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -5,7 +5,7 @@ Settings = {
   ---@type "traditional" | "harpoon"
   buffers_management = "harpoon",
 
-  --- Whether to use `none-ls` or `conform` + `nvim-lint` 
+  --- Whether to use `none-ls` or `conform` + `nvim-lint`
   ---@type boolean
   use_nls = true,
 }
