10b67f63d7de314ef2dfd6f5e158b20e82fc3fb6
xuanhung1509 <vuxuanhung15092002@gmail.com>
2024-06-30 10:53:25 +0700

    fix(telescope): add mouse scroll
    limit scroll
    use <C-u> to clear input

--- lua/user/plugins/configs/telescope.lua
+++ lua/user/plugins/configs/telescope.lua
@@ -9,6 +9,7 @@ return {
         selection_caret = "  ",
         borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
         sorting_strategy = "ascending",
+        scroll_strategy = "limit",
         layout_config = {
           width = 0.8,
           height = 0.8,
@@ -29,8 +30,15 @@ return {
             ["<C-j>"] = actions.cycle_history_next,
             ["<C-k>"] = actions.cycle_history_prev,
             ["<C-cr>"] = actions.file_vsplit,
+            ["<C-u>"] = false,
+            ["<ScrollWheelUp>"] = actions.move_selection_previous,
+            ["<ScrollWheelDown>"] = actions.move_selection_next,
           },
-          n = { q = actions.close },
+          n = {
+            q = actions.close,
+            ["<ScrollWheelUp>"] = actions.move_selection_previous,
+            ["<ScrollWheelDown>"] = actions.move_selection_next,
+          },
         },
       },
       pickers = {
