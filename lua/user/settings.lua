71d78cb1b1c2c0b958fe6fdb3a99c5458c0d4d2b
xuanhung1509 <vuxuanhung15092002@gmail.com>
2024-07-07 12:37:17 +0700

    docs: update setting note

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -9,9 +9,21 @@ Settings = {
   --[[
  Whether to use `telescope.nvim` or `fzf-lua.nvim`
 
-`telescope.nvim` has better community support and integration with other plugins, such as `aerial`, `nvim-notify`...
-while `fzf-lua.nvim` is more performant, especially in large codebases
+`telescope.nvim`:
+- Pros:
+    Has better community support and integration with other plugins, such as `aerial`, `nvim-notify`
+- Cons:
+    Cannot get lazyloaded colorschemes
+    Limited support for scrolling results/preview window (until recently)
+
+`fzf-lua.nvim`:
+- Pros:
+    Is more performant, especially in large codebases
+    Enable toggle option (respect/ignore .gitignore) out of the box
+    Can get lazyloaded colorschemes
+- Cons:
+    Limited integration with some plugins
    ]]
   ---@type boolean
-  use_fzf = false,
+  use_fzf = true,
 }
