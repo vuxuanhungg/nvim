4c994f9909eb668095f192209f22520678a5eb70
xuanhung1509 <vuxuanhung15092002@gmail.com>
2024-07-07 08:57:49 +0700

    feat(treesitter): add extra textobjects

--- lua/user/plugins/editor/treesitter.lua
+++ lua/user/plugins/editor/treesitter.lua
@@ -4,6 +4,7 @@ return {
   dependencies = {
     "windwp/nvim-ts-autotag",
     "JoosepAlviste/nvim-ts-context-commentstring",
+    "nvim-treesitter/nvim-treesitter-textobjects",
   },
   config = function()
     require("nvim-treesitter.configs").setup({
@@ -39,6 +40,26 @@ return {
       context_commentstring = {
         enable = true,
         enable_autocmd = false,
+      },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment" },
+            ["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment" },
+            ["l="] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
+            ["r="] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
+            ["af"] = { query = "@function.outer", desc = "Select outter part of a function definition" },
+            ["if"] = { query = "@function.inner", desc = "Select inner part of a function definition" },
+            ["ac"] = { query = "@class.outer", desc = "Select outer part of a class" },
+            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
+            ["ai"] = { query = "@conditional.outer", desc = "Select outer part of a conditional" },
+            ["ii"] = { query = "@conditional.inner", desc = "Select inner part of a conditional" },
+            ["al"] = { query = "@loop.outer", desc = "Select outer part of a loop" },
+            ["il"] = { query = "@loop.inner", desc = "Select inner part of a loop" },
+          },
+        },
       },
     })
   end,
