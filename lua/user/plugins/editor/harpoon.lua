ee4bb0cb57187bd23229ac4737d9467b0372908c
xuanhung1509 <vuxuanhung15092002@gmail.com>
2024-07-07 16:43:02 +0700

    fix(harpoon): don't lazyload on keymaps
    this causes initial Ctrl+i mapping to <tab>
    so I set event to VeryLazy instead

--- lua/user/plugins/editor/harpoon.lua
+++ lua/user/plugins/editor/harpoon.lua
@@ -11,20 +11,6 @@ return {
     })
 
     local gmap = require("user.utils").map()
-    local navigate_to_buffer = function(index)
-      harpoon:list():select(index)
-    end
-
-    gmap("n", "<C-i>", "<C-i>", "Keep Ctrl+i functionality when press Tab")
-
-    for i = 1, 5 do
-      gmap("n", "<leader>b" .. i, function()
-        navigate_to_buffer(i)
-      end, "Navigate to buffer " .. i)
-    end
-  end,
-  keys = function()
-    local harpoon = require("harpoon")
     local toggle_menu = function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
     end
@@ -32,10 +18,19 @@ return {
       harpoon:list():add()
       require("user.utils").notify("Buffer marked")
     end
+    local navigate_to_buffer = function(index)
+      harpoon:list():select(index)
+    end
 
-    return {
-      { "<tab>", toggle_menu, desc = "Show harpoon list" },
-      { "<leader>ba", mark_file, desc = "Mark buffer" },
-    }
+    gmap("n", "<C-i>", "<C-i>", "Keep Ctrl+i functionality when press Tab")
+    gmap("n", "<tab>", toggle_menu, "Show harpoon list")
+    gmap("n", "<leader>ba", mark_file, "Mark buffer")
+
+    for i = 1, 5 do
+      gmap("n", "<leader>b" .. i, function()
+        navigate_to_buffer(i)
+      end, "Navigate to buffer " .. i)
+    end
   end,
+  event = "VeryLazy",
 }
