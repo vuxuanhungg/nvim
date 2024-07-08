43685a3d4da7c97ecbc222cbb844d7d1d097af79
xuanhung1509 <vuxuanhung15092002@gmail.com>
2024-07-08 23:49:21 +0700

    fix!: update nvim-lint and its config
    we use eslint-lsp now,
    and it has diagnostics and code actions capabilities
    so no need to config nvim-lint for js/ts... files

--- lua/user/plugins/lsp/nvim-lint.lua
+++ lua/user/plugins/lsp/nvim-lint.lua
@@ -1,20 +1,11 @@
---[[
-Note: `eslint_d` cannot auto switch between (new) flat and old config format
-inside a monorepo where each repo uses a different config format
+local linters_by_ft = {}
 
-And I cannot get `nvim-lint` to work with `eslint` yet
---]]
 return {
   "mfussenegger/nvim-lint",
   config = function()
     local lint = require("lint")
 
-    lint.linters_by_ft = {
-      javascript = { "eslint_d" },
-      javascriptreact = { "eslint_d" },
-      typescript = { "eslint_d" },
-      typescriptreact = { "eslint_d" },
-    }
+    lint.linters_by_ft = linters_by_ft
 
     vim.api.nvim_create_autocmd({ "BufReadPost", "BufWritePost", "InsertLeave" }, {
       callback = function()
@@ -23,5 +14,5 @@ return {
     })
   end,
   event = { "BufReadPre", "BufNewFile" },
-  enabled = not Settings.use_nls,
+  enabled = not Settings.use_nls and #linters_by_ft > 0,
 }
