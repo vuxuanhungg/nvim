6d31a17570e214b94579a57a83e57869efd598d6
xuanhung1509 <vuxuanhung15092002@gmail.com>
2024-07-09 22:39:24 +0700

    fix(conform): update version and config

--- lua/user/plugins/lsp/conform.lua
+++ lua/user/plugins/lsp/conform.lua
@@ -2,26 +2,36 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
+      -- lua
       lua = { "stylua" },
+
+      -- bash
       sh = { "shfmt" },
-      python = { "black" },
+
+      -- web dev
       html = { { "prettierd", "prettier" } },
       css = { { "prettierd", "prettier" } },
       scss = { { "prettierd", "prettier" } },
       json = { { "prettierd", "prettier" } },
-      yaml = { { "prettierd", "prettier" } },
-      markdown = { { "prettierd", "prettier" } },
       javascript = { { "prettierd", "prettier" } },
       javascriptreact = { { "prettierd", "prettier" } },
       typescript = { { "prettierd", "prettier" } },
       typescriptreact = { { "prettierd", "prettier" } },
       vue = { { "prettierd", "prettier" } },
+
+      -- python
+      python = { "black" },
+
+      -- others
+      markdown = { { "prettierd", "prettier" } },
+      yaml = { { "prettierd", "prettier" } },
     },
     format_on_save = {
       timeout_ms = 500,
-      lsp_fallback = true,
+      lsp_format = "fallback",
     },
   },
-  event = { "BufReadPost", "BufNewFile" },
+  cmd = { "ConformInfo" },
+  event = { "BufWritePre" },
   enabled = not Settings.use_nls,
 }
