8278e31a765896555164e7c6b206ed1c4ffb0d5e
xuanhung1509 <vuxuanhung15092002@gmail.com>
2024-07-10 20:05:22 +0700

    fix(basedpyright): add keymap to organize imports

--- lua/user/plugins/lsp/servers/basedpyright.lua
+++ lua/user/plugins/lsp/servers/basedpyright.lua
@@ -2,7 +2,11 @@ return {
   setup = function(capabilities, common_on_attach)
     require("lspconfig")["basedpyright"].setup({
       capabilities = capabilities,
-      on_attach = common_on_attach,
+      on_attach = function(client, bufnr)
+        common_on_attach(client, bufnr)
+
+        vim.keymap.set("n", "<A-O>", "<cmd> PyrightOrganizeImports <cr>", { desc = "Organize imports", buffer = bufnr })
+      end,
       settings = {
         basedpyright = {
           analysis = {
