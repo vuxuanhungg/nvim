7a3e47d8261ef7de00b8d8b8fec6aa08132912fd
xuanhung1509 <vuxuanhung15092002@gmail.com>
2024-07-12 23:33:58 +0700

    fix: make q close spectre

--- lua/user/core/autocmds/neovim.lua
+++ lua/user/core/autocmds/neovim.lua
@@ -41,12 +41,12 @@ autocmd("FileType", {
 })
 
 autocmd("BufWinEnter", {
-  desc = "Make q close help, quickfix, notify, fugitive",
+  desc = "Make q close help, quickfix, notify, fugitive, spectre",
   group = augroup("q_close_windows", { clear = true }),
   callback = function(args)
     local buftype = vim.api.nvim_get_option_value("buftype", { buf = args.buf })
 
-    if vim.tbl_contains({ "help", "quickfix", "nofile", "nowrite" }, buftype) and vim.fn.maparg("q", "n") == "" then
+    if vim.tbl_contains({ "help", "quickfix", "nofile", "nowrite", "" }, buftype) and vim.fn.maparg("q", "n") == "" then
       vim.keymap.set("n", "q", "<cmd> close <cr>", {
         desc = "Close window",
         buffer = args.buf,
