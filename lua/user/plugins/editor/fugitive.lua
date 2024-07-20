2bc2bf1f285e40da0eccb1c5c3fdfd61c0b44e79
xuanhung1509 <vuxuanhung15092002@gmail.com>
2024-07-20 18:03:26 +0700

    fix(fugitive): remove barely used cmds

--- lua/user/plugins/editor/fugitive.lua
+++ lua/user/plugins/editor/fugitive.lua
@@ -1,30 +1,6 @@
 return {
   "tpope/vim-fugitive",
-
-  config = function()
-    ---------- Extended ----------
-    vim.api.nvim_create_user_command("Ghistory", function()
-      vim.cmd("vert G log -p " .. vim.api.nvim_buf_get_name(0))
-    end, { desc = "View commit history of current buffer in a vertical split" })
-  end,
-
-  cmd = {
-    "G",
-    "Git",
-    "Gdiffsplit",
-    "Gread",
-    "Gwrite",
-    "Ggrep",
-    "GMove",
-    "GDelete",
-    "GBrowse",
-    "GRemove",
-    "GRename",
-    "Glgrep",
-    "Gedit",
-    "Ghistory",
-  },
-
+  cmd = { "G", "Git", "Gread", "Gwrite" },
   keys = function()
     local toggle_fugitive = function()
       if not require("user.utils").is_git_repo() then
