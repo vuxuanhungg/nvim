0b0831c0103715aab644f296af8c5dcbf28f7c46
hungvx <hungvx@thanhphat.co>
2026-02-05 16:49:17 +0700

    fix: remove rainbow-delimiters
    I like similar elements in html and jsx tags
    have the same colors instead.
    This plugin makes code look eye-candy,
    but causes dev a headache.

--- lua/community.lua
+++ lua/community.lua
@@ -16,7 +16,6 @@ return {
   { import = "astrocommunity.pack.docker" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.recipes.vscode-icons" },
-  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
   { import = "astrocommunity.editing-support.nvim-treesitter-context" },
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.colorscheme.vscode-nvim" },
