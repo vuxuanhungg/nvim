97e065b3ab585d18edfaad185ea06e3f452828bb
hungvx <hungvx@thanhphat.co>
2026-02-24 17:20:56 +0700

    fix: update mason UI

--- lua/plugins/override.lua
+++ lua/plugins/override.lua
@@ -1,10 +1,22 @@
+---@type LazySpec
 return {
-  -- Gitsigns
-  "lewis6991/gitsigns.nvim",
-  opts = {
-    current_line_blame = true,
-    current_line_blame_opts = {
-      delay = 250,
+  ----- Override -----
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ui = {
+        border = "rounded",
+        height = 0.8,
+      },
+    },
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      current_line_blame = true,
+      current_line_blame_opts = {
+        delay = 250,
+      },
     },
   },
 }
