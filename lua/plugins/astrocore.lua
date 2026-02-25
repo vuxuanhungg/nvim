fab51c0747028296ae00e55e5ffff4f1af6d6d62
hungvx <hungvx@thanhphat.co>
2026-02-25 14:14:51 +0700

    refactor: cleanup sample code in astrocore.lua

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,87 +1,18 @@
--- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
--- Configuration documentation can be found with `:h astrocore`
--- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
---       as this provides autocomplete and documentation while editing
-
 ---@type LazySpec
 return {
   "AstroNvim/astrocore",
   ---@type AstroCoreOpts
   opts = {
-    -- Configure core features of AstroNvim
-    features = {
-      large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
-      autopairs = true, -- enable autopairs at start
-      cmp = true, -- enable completion at start
-      diagnostics = { virtual_text = false, virtual_lines = false }, -- diagnostic settings on startup
-      highlighturl = true, -- highlight URLs at start
-      notifications = true, -- enable notifications at start
-    },
-    -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
     diagnostics = {
-      virtual_text = true,
-      underline = true,
+      virtual_text = false,
     },
-    -- passed to `vim.filetype.add`
-    filetypes = {
-      -- see `:h vim.filetype.add` for usage
-      extension = {
-        foo = "fooscript",
-      },
-      filename = {
-        [".foorc"] = "fooscript",
-      },
-      pattern = {
-        [".*/etc/foo/.*"] = "fooscript",
-      },
-    },
-    -- vim options can be configured here
     options = {
-      opt = { -- vim.opt.<key>
-        relativenumber = true, -- sets vim.opt.relativenumber
-        number = true, -- sets vim.opt.number
-        spell = false, -- sets vim.opt.spell
-        signcolumn = "yes", -- sets vim.opt.signcolumn to yes
-        wrap = true, -- sets vim.opt.wrap
-      },
-      g = { -- vim.g.<key>
-        -- configure global vim variables (vim.g)
-        -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
-        -- This can be found in the `lua/lazy_setup.lua` file
+      opt = {
+        wrap = true,
       },
     },
-    -- Mappings can be configured through AstroCore as well.
-    -- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
     mappings = {
-      -- first key is the mode
       n = {
-        -- second key is the lefthand side of the map
-
-        -- navigate buffer tabs
-        ["]b"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
-        ["[b"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
-
-        -- mappings seen under group name "Buffer"
-        ["<Leader>bd"] = {
-          function()
-            require("astroui.status.heirline").buffer_picker(
-              function(bufnr) require("astrocore.buffer").close(bufnr) end
-            )
-          end,
-          desc = "Close buffer from tabline",
-        },
-
-        -- tables with just a `desc` key will be registered with which-key if it's installed
-        -- this is useful for naming menus
-        -- ["<Leader>b"] = { desc = "Buffers" },
-
-        -- setting a mapping to false will disable it
-        -- ["<C-S>"] = false,
-        ["[c"] = {
-          function() require("treesitter-context").go_to_context(vim.v.count1) end,
-          desc = "Jump to upward context",
-          silent = true,
-        },
         ["<C-p>"] = { function() require("snacks.picker").smart() end, desc = "Smart find Files" },
         ["J"] = { "mzJ`z", desc = "Retain cursor position on line join" },
         ["W"] = { ":noautocmd w<CR>", desc = "Save without formatting" },
@@ -89,26 +20,30 @@ return {
         ["<A-k>"] = { ":m .-2<CR>==", desc = "Move line up", silent = true },
         ["<A-J>"] = { "yyp", desc = "Copy line down", silent = true },
         ["<A-K>"] = { "yyP", desc = "Copy line up", silent = true },
+        ["[c"] = {
+          function() require("treesitter-context").go_to_context(vim.v.count1) end,
+          desc = "Jump to upward context",
+          silent = true,
+        },
       },
       v = {
-        ["<"] = { "<gv", desc = "Indent left" },
-        [">"] = { ">gv", desc = "Indent right" },
         ["<A-j>"] = { ":m '>+1<CR>gv-gv", desc = "Move block down", silent = true },
         ["<A-k>"] = { ":m '<-2<CR>gv-gv", desc = "Move block up", silent = true },
         ["<A-J>"] = { "yPgv", desc = "Copy block down", silent = true },
         ["<A-K>"] = { "y'>pgv", desc = "Copy block up", silent = true },
+        ["<"] = { "<gv", desc = "Indent left" },
+        [">"] = { ">gv", desc = "Indent right" },
       },
     },
     autocmds = {
       restore_session = {
+        -- https://docs.astronvim.com/recipes/sessions/#automatically-restore-previous-session
         {
           event = "VimEnter",
           desc = "Restore previous directory session if neovim opened with no arguments",
-          nested = true, -- trigger other autocommands as buffers open
+          nested = true,
           callback = function()
-            -- Only load the session if nvim was started with no args
             if vim.fn.argc(-1) == 0 then
-              -- try to load a directory session using the current working directory
               require("resession").load(vim.fn.getcwd(), { dir = "dirsession", silence_errors = true })
             end
           end,
