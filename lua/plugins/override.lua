46bc510cbbe2df4c73ca65e14cad7cfba3367338
hungvx <hungvx@thanhphat.co>
2026-02-28 16:38:33 +0700

    fix: extend jsx/tsx snippets to js/ts

--- lua/plugins/override.lua
+++ lua/plugins/override.lua
@@ -27,6 +27,14 @@ return {
       },
     },
   },
+  {
+    "L3MON4D3/LuaSnip",
+    config = function(plugin, opts)
+      require("luasnip").filetype_extend("javascript", { "javascriptreact" })
+      require("luasnip").filetype_extend("typescript", { "typescriptreact" })
+      require "astronvim.plugins.configs.luasnip"(plugin, opts)
+    end,
+  },
 
   ----- Custom -----
   {
