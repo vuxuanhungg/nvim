5c8f4d4cf5fa8b44b5c95015a26cd7d6c6b7e2ae
hungvx <hungvx@thanhphat.co>
2026-03-03 14:04:10 +0700

    fix: update snacks.picker keymaps
    - use Ctrl+Enter to open vsplit
    - use Ctrl+j/k to cycle search history

--- lua/plugins/override.lua
+++ lua/plugins/override.lua
@@ -71,6 +71,22 @@ return {
       },
     },
   },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        win = {
+          input = {
+            keys = {
+              ["<C-CR>"] = { "edit_vsplit", mode = { "i", "n" } },
+              ["<C-j>"] = { "history_forward", mode = { "i", "n" } },
+              ["<C-k>"] = { "history_back", mode = { "i", "n" } },
+            },
+          },
+        },
+      },
+    },
+  },
 
   ----- Custom -----
   {
